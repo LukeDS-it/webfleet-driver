ec7203b512f8857cd40e5a4be64ca6fc2e9ff101
Luca Di Stefano <luca.mcgyver@gmail.com>
2018-11-16 22:17:16 +0100

    fixing triggering wrong configuration on pull requests

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH = "master" ]; then
+if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST = "false"]; then
     git checkout master
     sbt 'release with-defaults'
     git remote add origin-release https://${GITHUB_TOKEN}@github.com/LukeDS-it/webfleet-driver.git > /dev/null
