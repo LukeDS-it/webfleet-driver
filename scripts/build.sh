7c86348b533123f201257dd0124983e4dfbf1406
Luca Di Stefano <luca.mcgyver@gmail.com>
2018-11-16 23:05:43 +0100

    fix shell script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "branch=$TRAVIS_BRANCH"
 echo "pr=$TRAVIS_PULL_REQUEST"
-if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST = "false"]
-then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     git checkout master
     sbt 'release with-defaults'
     git remote add origin-release https://${GITHUB_TOKEN}@github.com/LukeDS-it/webfleet-driver.git > /dev/null
