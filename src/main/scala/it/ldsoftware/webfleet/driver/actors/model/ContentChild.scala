b6c8c5c7a8fe3084675dd7dd2e029ca2eaa02c25
Luca Di Stefano <luca.mcgyver@gmail.com>
2020-05-21 17:47:42 +0200

    Added a way to get parent path from a child

--- src/main/scala/it/ldsoftware/webfleet/driver/actors/model/ContentChild.scala
+++ src/main/scala/it/ldsoftware/webfleet/driver/actors/model/ContentChild.scala
@@ -1,3 +1,5 @@
 package it.ldsoftware.webfleet.driver.actors.model
 
-case class ContentChild(path: String, title: String, description: String, webType: WebType)
+case class ContentChild(path: String, title: String, description: String, webType: WebType) {
+  def getParentPath: String = path.substring(0, path.lastIndexOf("/"))
+}
