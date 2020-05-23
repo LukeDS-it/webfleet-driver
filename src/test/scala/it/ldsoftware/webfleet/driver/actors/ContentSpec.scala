4e27f2686d5d9cb079def3262e75858fc1bf097e
Luca Di Stefano <luca.mcgyver@gmail.com>
2020-05-23 18:49:39 +0200

    Content unit testing

--- src/test/scala/it/ldsoftware/webfleet/driver/actors/ContentSpec.scala
+++ src/test/scala/it/ldsoftware/webfleet/driver/actors/ContentSpec.scala
@@ -3,6 +3,7 @@ package it.ldsoftware.webfleet.driver.actors
 import java.time.ZonedDateTime
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorRef
 import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
 import com.typesafe.config.{Config, ConfigFactory}
 import it.ldsoftware.webfleet.driver.actors.Content._
@@ -32,6 +33,8 @@ class ContentSpec
     rootTestKit.clear()
   }
 
+  private val Venice = (45.438759, 12.327145)
+
   "An empty content" should {
 
     "return not found when asked for its state" in {
@@ -43,13 +46,151 @@ class ContentSpec
     "return done when creating a content, and save the correct status" in {
       val form = rootForm
       val user = superUser
+      val expectedRootContent = getExpectedContent(form, user, Published)
+
       val result = rootTestKit.runCommand[Response](Create(form, user, _))
+
       result.reply shouldBe Done
       result.event shouldBe Created(form, user, testTime)
+      result.state shouldBe Existing(expectedRootContent)
     }
 
-    "do?" in {}
+    "return validation errors when content is invalid" in {
+      val form = rootForm.copy(path = "invalid path")
+      val user = superUser
 
+      val result = rootTestKit.runCommand[Response](Create(form, user, _))
+
+      val pathLocation =
+        ValidationError("path", "Path is not the same as http path", "path.location")
+
+      val pathPattern =
+        ValidationError("path", "Path cannot contain symbols except - and _", "path.pattern")
+
+      result.reply shouldBe Invalid(List(pathLocation, pathPattern))
+      result.events shouldBe empty
+    }
+
+  }
+
+  "An existing content" should {
+
+    "return its contents when asked" in {
+      val form = rootForm
+      val user = superUser
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](Read)
+
+      result.reply shouldBe MyContent(getExpectedContent(form, user, Published))
+    }
+
+    "return a validation error if trying to create an already existing content" in {
+      val form = rootForm
+      val user = superUser
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](Create(form, user, _))
+
+      val alreadyExists = ValidationError("path", s"Content at / already exists", "path.duplicate")
+
+      result.reply shouldBe Invalid(List(alreadyExists))
+    }
+
+    "correctly update its contents" in {
+      val form = rootForm
+      val user = superUser
+      val update = UpdateForm(title = Some("New title"))
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+
+      val result = rootTestKit.runCommand[Response](Update(update, user, _))
+
+      result.reply shouldBe Done
+      result.event shouldBe Updated(update, user, testTime)
+      result.state.asInstanceOf[Existing].webContent.title shouldBe "New title"
+    }
+
+    "return a validation error if the edits are invalid" in {
+      val form = rootForm
+      val user = superUser
+      val update =
+        UpdateForm(event = Some(WebCalendar(testTime, testTime.plusHours(1), "Venice", Venice)))
+
+      val expectedErrors = List(
+        ValidationError(
+          "event",
+          "Cannot insert an event in a non-calendar content",
+          "content.notCalendar"
+        )
+      )
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](Update(update, user, _))
+
+      result.reply shouldBe Invalid(expectedErrors)
+    }
+
+    "delete itself correctly" in {
+      val form = rootForm
+      val user = superUser
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](Delete(user, _))
+      result.reply shouldBe Done
+      result.event shouldBe Deleted(user, testTime)
+      result.state shouldBe NonExisting("/")
+
+      rootTestKit.runCommand[Response](Read).reply shouldBe NotFound("/")
+    }
+
+    "correctly add a child" in {
+      val form = rootForm
+      val user = superUser
+      val child = rootForm.copy(path = "/child", title = "Child").toChild
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](AddChild(child, _))
+      result.reply shouldBe Done
+      result.event shouldBe ChildAdded(child)
+      result.state.asInstanceOf[Existing].webContent.children should contain(child.path -> child)
+    }
+
+    "correctly update a child" in {
+      val form = rootForm
+      val user = superUser
+      val child = rootForm.copy(path = "/child", title = "Child").toChild
+      val child2 = child.copy(title = "New title")
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      rootTestKit.runCommand[Response](AddChild(child, _))
+      val result = rootTestKit.runCommand[Response](UpdateChild(child2, _))
+
+      result.reply shouldBe Done
+      result.event shouldBe ChildUpdated(child2)
+      result.state.asInstanceOf[Existing].webContent.children should contain(child.path -> child2)
+    }
+
+    "correctly remove a child" in {
+      val form = rootForm
+      val user = superUser
+      val child = rootForm.copy(path = "/child", title = "Child").toChild
+
+      rootTestKit.runCommand[Response](Create(form, user, _))
+      val result = rootTestKit.runCommand[Response](RemoveChild(child.path, _))
+      result.reply shouldBe Done
+      result.event shouldBe ChildRemoved(child.path)
+      result.state.asInstanceOf[Existing].webContent.children shouldNot contain(child.path -> child)
+    }
+
+  }
+
+  "The init function" should {
+    "correctly initialize the actor" in {
+      val ref = Content.init(system)
+
+      ref shouldBe an[ActorRef[_]]
+    }
   }
 
   def rootForm: CreateForm = CreateForm(
@@ -65,6 +206,23 @@ class ContentSpec
   )
 
   def superUser: User = User("name", Permissions.all, None)
+
+  def getExpectedContent(form: CreateForm, user: User, expectedStatus: ContentStatus): WebContent =
+    WebContent(
+      form.title,
+      form.path,
+      form.webType,
+      form.description,
+      form.text,
+      form.theme,
+      form.icon,
+      form.event,
+      expectedStatus,
+      user.name,
+      Some(testTime),
+      Some(testTime),
+      Map()
+    )
 
 }
 
