3322137c61db58e813fabf46bd1a5f2a24fadbf6
Luca Di Stefano <luca.mcgyver@gmail.com>
2020-05-29 16:24:45 +0200

    fixing compilation errors

--- src/main/scala/it/ldsoftware/webfleet/driver/actors/EventProcessor.scala
+++ src/main/scala/it/ldsoftware/webfleet/driver/actors/EventProcessor.scala
@@ -1,12 +1,11 @@
 package it.ldsoftware.webfleet.driver.actors
 
-import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 
 object EventProcessor {
 
   def init(): Unit = {}
 
-  def apply(): Behavior[Nothing] = Behaviors.setup { ctx => Behaviors.same }
+  def apply() = Behaviors.setup { ctx => Behaviors.same }
 
 }
