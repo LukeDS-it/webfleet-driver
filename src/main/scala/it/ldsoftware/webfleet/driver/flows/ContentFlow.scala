e4f1bb8b9ec0777af467137adb04f1735bb45ba0
Luca Di Stefano <luca.mcgyver@gmail.com>
2020-05-29 16:24:28 +0200

    fixing compilation errors

--- src/main/scala/it/ldsoftware/webfleet/driver/flows/ContentFlow.scala
+++ src/main/scala/it/ldsoftware/webfleet/driver/flows/ContentFlow.scala
@@ -1,7 +1,7 @@
 package it.ldsoftware.webfleet.driver.flows
 
 import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
-import akka.persistence.query.EventEnvelope
+import akka.persistence.query.{EventEnvelope, Offset}
 import akka.stream.scaladsl.{Sink, Source}
 import akka.{Done, NotUsed}
 import it.ldsoftware.webfleet.driver.actors.Content
@@ -23,9 +23,9 @@ class ContentFlow(readJournal: JdbcReadJournal, db: Database, consumers: Seq[Con
       }
       .map(_.getOrElse(0L))
 
-  def writeLastOffset(offset: Long): Future[Int] =
+  def writeLastOffset(offset: Offset): Future[Int] =
     db.run {
-      sqlu"update offset_store set last_offset = $offset where tag = $Tag"
+      sqlu"update offset_store set last_offset = 0 where tag = $Tag"
     }
 
   val input: Source[EventEnvelope, Future[NotUsed]] =
@@ -36,7 +36,9 @@ class ContentFlow(readJournal: JdbcReadJournal, db: Database, consumers: Seq[Con
   val consume: Sink[EventEnvelope, Future[Done]] =
     Sink.foreach[EventEnvelope] { env =>
       env.event match {
-        case x: Content.Event => consumers.foreach(_.consume(x))
+        case x: Content.Event =>
+          consumers.foreach(_.consume(x))
+          writeLastOffset(env.offset)
       }
     }
 
