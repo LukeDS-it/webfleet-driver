62ada192945b472f25f20386d13948838ccea77f
Luca Di Stefano <luca.mcgyver@gmail.com>
2020-06-15 18:10:34 +0200

    ran scalafmt

--- src/main/scala/it/ldsoftware/webfleet/driver/service/ContentService.scala
+++ src/main/scala/it/ldsoftware/webfleet/driver/service/ContentService.scala
@@ -10,9 +10,19 @@ trait ContentService {
 
   def getContent(domain: String, path: String): Future[ServiceResult[WebContent]]
 
-  def createContent(domain: String, path: String, form: CreateForm, user: User): Future[ServiceResult[String]]
+  def createContent(
+      domain: String,
+      path: String,
+      form: CreateForm,
+      user: User
+  ): Future[ServiceResult[String]]
 
-  def editContent(domain: String, path: String, form: UpdateForm, user: User): Future[ServiceResult[NoResult]]
+  def editContent(
+      domain: String,
+      path: String,
+      form: UpdateForm,
+      user: User
+  ): Future[ServiceResult[NoResult]]
 
   def deleteContent(domain: String, path: String, user: User): Future[ServiceResult[NoResult]]
 
